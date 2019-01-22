97db49a153f020100f2f2f02aa3e156f39486fcd
Glitch (catfact) <none>
2019-01-22 02:22:32 +0000

    💠🚉 Updated with Glitch

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+
+const http = require('http');
+const express = require('express');
+const app = express();
+
 //const settings = require('./settings.json');
+
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+  //response.sendFile(__dirname + '/site/index.html');
+  
+  //app.use(morgan('combined'))
+});
+
+app.get('/', function (req, res) {
+  res.send('hello, world!')
+})
+
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
 
 client.on('ready',() => {
   console.log('I\'m Online\nI\'m Online');
